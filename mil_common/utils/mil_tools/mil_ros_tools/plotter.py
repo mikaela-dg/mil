52886533041803c5b2926ca2c1f5c78e3e71b4ac
MarshallRawson <marshallrawson@ufl.edu>
2020-02-15 14:17:59 -0500

    TOOLS: Plotter: docstring added, lazy publisher

--- mil_common/utils/mil_tools/mil_ros_tools/plotter.py
+++ mil_common/utils/mil_tools/mil_ros_tools/plotter.py
@@ -9,7 +9,53 @@ from matplotlib.figure import Figure
 import matplotlib
 import threading
 
-class MilRosPlotter:
+class Plotter:
+    '''Publishes several plots of 2d data over rostopic via Image msgs
+    Basic Usage Example:
+        #  Import
+        from mil_ros_tools import Plotter
+        import numpy as np
+        import rospy
+
+        #  ROS node
+        rospy.init_node('my_node')
+
+        #  Create
+        my_plotter = Plotter('my_plotter_topic')
+
+        #  Publish some plots
+        titles = ['random data', 'sin wave']
+
+        data_size = 100
+        y1 = np.random.rand(data_size)
+        x1 = np.arange(0, y1.shape[0])
+
+        x2 = np.linspace(0, 5, data_size)
+        y2 = np.sin(x2)
+
+        plots = np.vstack((x1, y1, x2, y2))
+        my_plotter.publish_plots(plots, titles)
+
+    For another usage example see `mil_passive_sonar triggering`
+
+    Limitations:
+        can only stack plots vertially
+
+        all plots must have same number of points
+
+        cannot name axes
+
+        Publishing happens in another thread,
+            if publish_plots is called before a previous publish plots call finishes,
+            the most recent publish plots call will be ignored
+
+        cannot plot mutiple data sets on top of  each other in the same plot
+
+        cannot change color of plots
+
+    Features:
+        Can be enables/disabled via the enable_<topic_name> service call
+    '''
 
     def __init__(self, topic_name, w=20, h=20, dpi=200):
         matplotlib.rcParams.update({'font.size': 22})
@@ -29,6 +75,8 @@ class MilRosPlotter:
 
 
     def publish_plots(self, plots, titles=[]):
+        if self.pub.get_num_connections() == 0:
+            return
         if not self.enabled:
             return
         if (self.thread is not None) and self.thread.is_alive():
